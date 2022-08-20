ca2599578e5bf7f3cc5bf2242548bc321861e489
phoreka <irfan398649@gmail.com>
2022-08-21 01:21:11 +0530

    Folder Structure for Routes

--- server/index.js
+++ server/index.js
@@ -1,31 +1,30 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
-const app = express()
+const app = express();
 
 // https://localhost:5000/posts
-app.use('/posts',postRoutes)
 
-app.use(bodyParser.json({limit: "30m", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30m", extended: true}))
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json({ limit: "30m", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30m", extended: true }));
+app.use("/posts", postRoutes);
 
 //MongoDB
-/*
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
 
-*/
-
-const CONNECTION_URL = "mongodb+srv://memories_user:karomaro@cluster0.qbie7i4.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL =
+  "mongodb+srv://memories_user:karomaro@cluster0.qbie7i4.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>app.listen(PORT, ()=>console.log(`Server Running on Port: https://localhost:${PORT}`)))
-.catch((error)=>console.log(`${error} did not connect`))
-
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: https://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
